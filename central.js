0b3e453fe3c4bfe4a70867b3d0987b5423be5101
klausmayr <55473722+klausmayr@users.noreply.github.com>
2021-06-06 12:58:48 -0400

    Add files via upload

---
+++ central.js
@@ -0,0 +1,12 @@
+var centrales = {
+"type": "FeatureCollection",
+"name": "central",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "name": "Puerto Real", "owner": null, "prod_1907": null, "prod_1910": null }, "geometry": null },
+{ "type": "Feature", "properties": { "id": 2, "name": "Playa Grande", "owner": "Benítez Sugar Co.", "prod_1907": 2984, "prod_1910": 4366 }, "geometry": { "type": "Point", "coordinates": [ -65.519875467899354, 18.094730948334323 ] } },
+{ "type": "Feature", "properties": { "id": 3, "name": "Arkadia", "owner": "Arkadia Sugar Co.", "prod_1907": 1500, "prod_1910": 2437 }, "geometry": { "type": "Point", "coordinates": [ -65.543082312518678, 18.112546462308067 ] } },
+{ "type": "Feature", "properties": { "id": 4, "name": "Esperanza (Puerto Real)", "owner": "Victor Mourraille", "prod_1907": 2056, "prod_1910": 4280 }, "geometry": { "type": "Point", "coordinates": [ -65.470982964990554, 18.095936944466782 ] } },
+{ "type": "Feature", "properties": { "id": 5, "name": "Santa María", "owner": "Charles Le Brun", "prod_1907": 1130, "prod_1910": 1746 }, "geometry": { "type": "Point", "coordinates": [ -65.432773635065672, 18.153664510397515 ] } }
+]
+}
